6125966b7cbee65ec4603a95bcd9eed424386da5
InternDN19.01.03 Nguyen Thi Hong Dao <InternDN19.01.03@logigear.com>
2019-04-05 18:13:21 +0700

    start

---
+++ robot_project/src/main/java/com/robot/tests/robot_project/App.java
@@ -0,0 +1,13 @@
+package com.robot.tests.robot_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
